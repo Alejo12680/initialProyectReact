fc9f0c2fc3add46cbaf594d7802b2d28ff6ac22b
Alejo12680 <alejo152725@gmail.com>
2024-05-15 23:37:16 -0500

    examplo_funtion_states_react

--- src/Component.js
+++ src/Component.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Component() {
+
+    // Constante que se usan para utilizar el (useState) estados a un componenete, el cual consta de un array con dos elementos: El estado actua y La función set.
+    const [text, setText] = useState();
+    const [update, setUpdate] = useState();
+
+    // Creamos las funciones del set, te permite actualizar el estado a un valor diferente y desencadenar un nuevo renderizado.
+    const textOnChange = (event) => {
+        setText(event.target.value);
+    }
+
+    const buttonOnClick = () => {
+        setUpdate(text)
+    }
+
   return (
-    <div>My Component Firts</div>
+    <div>
+        {/* En el input el value tiene la variable "text" y utilizamos la funcion onChange para escuchar los cambios en el cual se ejecuta la funcion set "textOnChange" */}
+        <input type="text" value={text} onChange={textOnChange}/>
+
+        {/* En el button colocamos la funcion onClic para poder generar los cambios, en el cual se ejecuta la funcion set "buttonOnClick" */}
+        <button onClick={buttonOnClick}>Actualizar</button>
+
+        {/* En la P vamos a interpolar el texto que se escribio en el input gracias al (onChanger) */}
+        <p>Texto input: <b> {text} </b> </p>
+        <p>Texto Actualizado: <u> {update} </u></p>
+    </div>
   )
 }
